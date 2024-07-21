4a81ce36762176b4d57063f84080d7730a21b2eb
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-07-21 11:21:29 -0500

    first

---
+++ android/app/src/main/kotlin/com/example/providerappg8/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.providerappg8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:providerappg8/home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
